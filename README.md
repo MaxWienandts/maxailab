b06e7836b028cf72d9f8234ed32c24e2b3a7ffac
Max Wienandts <maxwienandts@gmail.com>
2026-03-01 06:46:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# stableml
+
+Bootstrap-based stability analysis toolkit for ML pipelines.
+
+## Installation
+
+pip install stableml
+
+## Example
+
+```python
+from stableml import bootstrap_lightgbm_forward_selection
---
+++ src/maxwailab/__init__.py
@@ -0,0 +1,10 @@
+from .binning import (
+    tree_supervised_binning,
+    bootstrap_tree_binning_auc_analysis,
+)
+
+from .feature_selection import (
+    bootstrap_lightgbm_forward_selection,
+    performance_forward_selection_boxplot,
+    variable_frequency_forward_selection,
+)
